d4c43b8dc849fff1205eee7167aad2cb3af00770
theconfusedindian <schandrapatla.ca@gmail.com>
2022-12-01 13:47:19 -0800

    final push

---
+++ src/Components/FormStep3/FormStep3.jsx
@@ -0,0 +1,112 @@
+import { Select, FormControl, InputLabel, MenuItem } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
+import "./FormStep3.scss";
+import design from "../../Assets/Images/frame-33.png";
+
+import Logo from "../Logo/Logo";
+
+export default function Welcome(props) {
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    navigate("/welcome");
+  }
+  return (
+    <div className="sign-up-biz">
+      <div className="form">
+        <form>
+          <Logo logoClassName="logo--form" />
+          <h1>Tell us about your business!!!</h1>
+          <p className="form__para--text">
+            We'd like to learn more about your business so that we can provide
+            you with the best experience possible
+          </p>
+          <div className="form__option">
+            <p className="form__para">
+              Select number of owners, employees, and contractors{" "}
+            </p>
+            <FormControl fullWidth>
+              <InputLabel className="form__label" id="demo-simple-select-label">
+                Company Size
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                // value={age}
+                label="Company Size"
+                // onChange={handleChange}
+              >
+                <MenuItem value={1}>1</MenuItem>
+                <MenuItem value={2 - 5}>2-5</MenuItem>
+                <MenuItem value={6 - 10}>6-10</MenuItem>
+                <MenuItem value={11 - 25}>11-25</MenuItem>
+                <MenuItem value={"26+"}>26+</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
+          <div className="form__option">
+            <p className="form__para">
+              Select average monthly revenue before deductions
+            </p>
+            <FormControl fullWidth>
+              <InputLabel className="form__label" id="demo-simple-select-label">
+                Monthly Revenue
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                // value={age}
+                label="Company Size"
+                // onChange={handleChange}
+              >
+                <MenuItem value={0}>$0 (Pre-revenue)</MenuItem>
+                <MenuItem value={"1,000"}> $1-$1,000</MenuItem>
+                <MenuItem value={"10,000"}>$1,001-$10,000</MenuItem>
+                <MenuItem value={"100,000"}>$10,001-$100,000</MenuItem>
+                <MenuItem value={"100,000+"}>More than $100,001</MenuItem>
+                <MenuItem value={"Unknown"}>I'm not sure</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
+          <div className="form__option">
+            <label htmlFor="tax-return">
+              <h3 className="form__label">Last Tax Return</h3>
+            </label>
+            <p className="form__para">
+              When did you file your last business tax return?
+            </p>
+            <FormControl fullWidth>
+              <InputLabel className="form__label" id="demo-simple-select-label">
+                Last Tax Return
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                // value={age}
+                label="Company Size"
+                // onChange={handleChange}
+              >
+                <MenuItem value={0}>I'm on track with my tax filing</MenuItem>
+                <MenuItem value={"1,000"}> Tax year 2021</MenuItem>
+                <MenuItem value={"10,000"}>Tax Year 2020</MenuItem>
+                <MenuItem value={"100,000"}>Tax year 2019 or prior</MenuItem>
+                <MenuItem value={"100,000+"}>My business is new</MenuItem>
+                <MenuItem value={"Unknown"}>
+                  Unknown/I need help figure this out
+                </MenuItem>
+              </Select>
+            </FormControl>
+          </div>
+          <div></div>
+        </form>
+        <button onClick={(e) => handleSubmit(e)} className="form__button">
+          Next
+        </button>
+      </div>
+      <div className="form__design">
+        <img src={design} alt="image" />
+      </div>
+    </div>
+  );
+}
---
+++ src/Pages/Welcome.jsx
@@ -0,0 +1,12 @@
+import backgroundPhoto from "../Assets/Images/WebAppBG.png";
+import "./Welcome.scss";
+
+const Welcome = () => {
+  return (
+    <div className="welcome">
+      <div className="modal"></div>
+    </div>
+  );
+};
+
+export default Welcome;
