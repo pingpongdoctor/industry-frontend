ac31005ef1e28964d49fb270ed361c6321a4832c
Simon Tran <thanhnhantran1501@gmail.com>
2022-12-01 12:32:45 -0800

    push form 1

--- src/Components/FormStep2/FormStep2.jsx
+++
@@ -1,8 +0,0 @@
-export default function FormStep2(props) {
-  return (
-    <div>
-      <h1>fnbreahjgnkresgnsengrsengiore</h1>
-      <button onClick={() => props.goToStep3()}></button>
-    </div>
-  );
-}
